ce69f49c3828a179cee63c528d188a1ff3ce2637
Dolan Pettit <dolanpettit@gmail.com>
2020-10-20 08:51:40 -0400

    Update: Deploying to Heroku

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+const Workout = require("./models/workoutLogic");
+
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
-// routes
-
-app.use(require("./routes/html-route"));
-app.use(require("./routes/api-route"));
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
